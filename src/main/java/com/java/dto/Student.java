4b868cf46a377ac9862cb22bdce5388d0fdc4202
payalbnsl <jahanvi.bansal@gmail.com>
2018-10-19 00:18:09 +0530

    v1

---
+++ src/main/java/com/java/dto/Student.java
@@ -0,0 +1,35 @@
+package com.java.dto;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
+@EqualsAndHashCode
+@Entity
+public class Student {
+	@GeneratedValue(strategy=GenerationType.AUTO)
+	@Id
+	private int id;
+	@Column(name="stName",unique=true)
+	private String name;
+	@OneToMany(mappedBy="student")
+	List<Phone> phone= new ArrayList<>();
+}
